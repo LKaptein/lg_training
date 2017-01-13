93ae934f339c9cf429e792da7490659129238c6e
Laurie Kaptein <laurie@limoengroen.nl>
2017-01-13 09:07:38 +0100

    Theming

---
+++ htdocs/files/php/twig/d870c560_page--front.html.twig_032ba3c4ab4204ee2d59e6000e8787b6c979640938a1fdd83bef5285ab56559a/843162db9dd571905b8e9cfacdf8fb44c08085f13fd8c4b4eeeaf712ca94bea9.php
@@ -0,0 +1,195 @@
+<?php
+
+/* themes/custom/eg/templates/page--front.html.twig */
+class __TwigTemplate_a8482e448b10534c95152d4d8d538a65b418ce922d2434d41c8d9b238f7c9853 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $tags = array("if" => 61);
+        $filters = array();
+        $functions = array();
+
+        try {
+            $this->env->getExtension('sandbox')->checkSecurity(
+                array('if'),
+                array(),
+                array()
+            );
+        } catch (Twig_Sandbox_SecurityError $e) {
+            $e->setTemplateFile($this->getTemplateName());
+
+            if ($e instanceof Twig_Sandbox_SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof Twig_Sandbox_SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+        // line 45
+        echo "<div class=\"layout-container\">
+
+  <header role=\"banner\">
+    ";
+        // line 48
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, $this->getAttribute((isset($context["page"]) ? $context["page"] : null), "header", array()), "html", null, true));
+        echo "
+  </header>
+
+
+  ";
+        // line 52
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, $this->getAttribute((isset($context["page"]) ? $context["page"] : null), "breadcrumb", array()), "html", null, true));
+        echo "
+
+  ";
+        // line 54
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, $this->getAttribute((isset($context["page"]) ? $context["page"] : null), "highlighted", array()), "html", null, true));
+        echo "
+
+  ";
+        // line 56
+        echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, $this->getAttribute((isset($context["page"]) ? $context["page"] : null), "help", array()), "html", null, true));
+        echo "
+
+  <main role=\"main\">
+    <a id=\"main-content\" tabindex=\"-1\"></a>";
+        // line 60
+        echo "
+    ";
+        // line 61
+        if ($this->getAttribute((isset($context["page"]) ? $context["page"] : null), "front", array())) {
+            // line 62
+            echo "      <div class=\"front\" class=\"content\">
+        ";
+            // line 63
+            echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, $this->getAttribute((isset($context["page"]) ? $context["page"] : null), "front", array()), "html", null, true));
+            echo "
+      </div>
+    ";
+        }
+        // line 66
+        echo "
+  </main>
+
+  ";
+        // line 69
+        if ($this->getAttribute((isset($context["page"]) ? $context["page"] : null), "footer", array())) {
+            // line 70
+            echo "    <footer role=\"contentinfo\">
+      ";
+            // line 71
+            echo $this->env->getExtension('sandbox')->ensureToStringAllowed($this->env->getExtension('drupal_core')->escapeFilter($this->env, $this->getAttribute((isset($context["page"]) ? $context["page"] : null), "footer", array()), "html", null, true));
+            echo "
+    </footer>
+  ";
+        }
+        // line 74
+        echo "
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "themes/custom/eg/templates/page--front.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  101 => 74,  95 => 71,  92 => 70,  90 => 69,  85 => 66,  79 => 63,  76 => 62,  74 => 61,  71 => 60,  65 => 56,  60 => 54,  55 => 52,  48 => 48,  43 => 45,);
+    }
+}
+/* {#*/
+/* /***/
+/*  * @file*/
+/*  * Theme override to display a single page.*/
+/*  **/
+/*  * The doctype, html, head and body tags are not in this template. Instead they*/
+/*  * can be found in the html.html.twig template in this directory.*/
+/*  **/
+/*  * Available variables:*/
+/*  **/
+/*  * General utility variables:*/
+/*  * - base_path: The base URL path of the Drupal installation. Will usually be*/
+/*  *   "/" unless you have installed Drupal in a sub-directory.*/
+/*  * - is_front: A flag indicating if the current page is the front page.*/
+/*  * - logged_in: A flag indicating if the user is registered and signed in.*/
+/*  * - is_admin: A flag indicating if the user has permission to access*/
+/*  *   administration pages.*/
+/*  **/
+/*  * Site identity:*/
+/*  * - front_page: The URL of the front page. Use this instead of base_path when*/
+/*  *   linking to the front page. This includes the language domain or prefix.*/
+/*  **/
+/*  * Page content (in order of occurrence in the default page.html.twig):*/
+/*  * - node: Fully loaded node, if there is an automatically-loaded node*/
+/*  *   associated with the page and the node ID is the second argument in the*/
+/*  *   page's path (e.g. node/12345 and node/12345/revisions, but not*/
+/*  *   comment/reply/12345).*/
+/*  **/
+/*  * Regions:*/
+/*  * - page.header: Items for the header region.*/
+/*  * - page.primary_menu: Items for the primary menu region.*/
+/*  * - page.secondary_menu: Items for the secondary menu region.*/
+/*  * - page.highlighted: Items for the highlighted content region.*/
+/*  * - page.help: Dynamic help text, mostly for admin pages.*/
+/*  * - page.content: The main content of the current page.*/
+/*  * - page.sidebar_first: Items for the first sidebar.*/
+/*  * - page.sidebar_second: Items for the second sidebar.*/
+/*  * - page.footer: Items for the footer region.*/
+/*  * - page.breadcrumb: Items for the breadcrumb region.*/
+/*  **/
+/*  * @see template_preprocess_page()*/
+/*  * @see html.html.twig*/
+/*  *//* */
+/* #}*/
+/* <div class="layout-container">*/
+/* */
+/*   <header role="banner">*/
+/*     {{ page.header }}*/
+/*   </header>*/
+/* */
+/* */
+/*   {{ page.breadcrumb }}*/
+/* */
+/*   {{ page.highlighted }}*/
+/* */
+/*   {{ page.help }}*/
+/* */
+/*   <main role="main">*/
+/*     <a id="main-content" tabindex="-1"></a>{# link is in html.html.twig #}*/
+/* */
+/*     {% if page.front %}*/
+/*       <div class="front" class="content">*/
+/*         {{ page.front }}*/
+/*       </div>*/
+/*     {% endif %}*/
+/* */
+/*   </main>*/
+/* */
+/*   {% if page.footer %}*/
+/*     <footer role="contentinfo">*/
+/*       {{ page.footer }}*/
+/*     </footer>*/
+/*   {% endif %}*/
+/* */
+/* </div>{# /.layout-container #}*/
+/* */
