b21012f468750171ccc08bef05695c19b0bbc4ed
Laurie Kaptein <laurie@limoengroen.nl>
2017-06-14 20:31:01 +0200

    page_router, move some code that will become a sevice.

--- htdocs/modules/custom/page_router/src/Controller/PageRouterController.php
+++ htdocs/modules/custom/page_router/src/Controller/PageRouterController.php
@@ -2,20 +2,52 @@
 
 namespace Drupal\page_router\Controller;
 
+use Drupal\page_router\PageRouterServices\PageMessageGenerator;
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Response;
 
-/**
- * Output for a page redirect to NewPage
- */
+//Step 1: print out message to page
+//class PageRouterController {
+//  public function myMessage($count) {
+//    $i=0;
+//    do {
+//      echo "count is ". $i . '<br/>';
+//      $i++;
+//    } while ($i < $count);
+//
+//    return new Response("Laurie's First Custom Page!!");
+//  }
+//}
+
+//Step 2: Move message to a new file that will become a service.
 class PageRouterController {
   public function myMessage($count) {
-    $i=0;
-    do {
-      echo "count is ". $i . '<br/>';
-      $i++;
-    } while ($i < $count);
 
-    return new Response("Laurie's First Custom Page!!");
+    $PageMessageGenerator = new PageMessageGenerator();
+    $message = $PageMessageGenerator->getMessage($count);
+    return new Response($message);
   }
-
 }
+
+//Step 3: Add Message Generator as a service
+//class PageRouterController extends ControllerBase {
+//
+//  private $PageMessageGenerator;
+//
+//  public function __construct(PageMessageGenerator $PageMessageGenerator) {
+//    $this->PageMessageGenerator = $PageMessageGenerator;
+//  }
+//
+//  public function myMessage ($count) {
+//
+//    $message = $PageMessageGenerator->->myMessage($count);
+//
+//    return new Response($message);
+//  }
+//
+//  public static function create(ContainerInterface $container) {
+//    $PageMessageGenerator = $container ->get('page_router.PageMessageGenerator');
+//    return new static ($PageMessageGenerator);
+//  }
+
--- htdocs/modules/custom/page_router/src/PageRouterServices/PageMessageGenerator.php
+++ htdocs/modules/custom/page_router/src/PageRouterServices/PageMessageGenerator.php
@@ -1,11 +1,17 @@
 <?php
 
+namespace Drupal\page_router\PageRouterServices;
 /**
- * Created by PhpStorm.
- * User: lauriekaptein
- * Date: 14/06/2017
- * Time: 19:40
+ * Generates a message and a counter printed out the number of times as the input paramter.
  */
 class PageMessageGenerator {
+  public function getMessage($count) {
 
+    $i = 0;
+    do {
+      echo "count is " . $i . '<br/>';
+      $i++;
+    } while ($i < $count);
+    return "Hello from Laurie!";
+  }
 }
